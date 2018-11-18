19080eb8d82c8e6f00db618c5752fb3f4635295f
Mike McAllister <mike@logikbox.com>
2018-11-18 09:16:32 -0500

    fixed mail sending issue

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -3,13 +3,13 @@ include_once("model/Model.php");
 
 class Controller {
 	public $model;
-	
-	public function __construct()  
-    {  
+
+	public function __construct()
+    {
         $this->model = new Model();
 
-    } 
-	
+    }
+
 	public function invoke()
 	{
 		if (!isset($_GET['job']))
@@ -25,14 +25,14 @@ class Controller {
 			include 'view/viewjob.php';
 		}
 	}
-	
+
 	public function nav()
 	{
 		$jobs = $this->model->getJobList();
 		include 'view/jobnav.php';
 	}
-	
-	
+
+
 	public function get_form(){
         $name = $_REQUEST['name'];
         $name = trim($name);
@@ -40,50 +40,42 @@ class Controller {
        // $msg = trim($msg);
 		$msg =  $name . " writes \r\n" . $msg;
 		$email_from = $_POST['email'];
-		$email_from = trim($email_from); 
-		
+		$email_from = trim($email_from);
+
 		//reCaptcha
 		$secret   = "6LfoV3sUAAAAABCL8io_gVio80cvcxbwzW0cm-XQ";
 		$response = $_POST['token'];
 		$remoteip = $_SERVER['REMOTE_ADDR'];
 		//$remoteip = "".mt_rand(0,255).".".mt_rand(0,255).".".mt_rand(0,255).".".mt_rand(0,255);
-
 		$recaptcha_response =  file_get_contents("https://www.google.com/recaptcha/api/siteverify?secret=$secret&response=$response&remoteip=$remoteip");
 		$result  = json_decode($recaptcha_response,true);
-		
-		//$data = '{ "success": true, "challenge_ts": "2018-11-17T21:05:04Z", "hostname": "localhost", "score": 0.9, "action": "email_forms" }';
-		//$data = '{"success": false,"race": "Human"}';
-		//$character = json_decode($data,true);
-		//echo "data: " . $character['success'] . "<hr>";
-		//echo "capt: " . $result['success'] . " - " . isset($result['success']). "<hr>";
-		//echo var_dump($result);
-		//$data = json_decode($recaptcha_response);
- 
+
+
 		if(isset($result['success'] ) &&  $result['success'] == 1) {
 
-             print_r("Thanks for reaching out. I'll get back to you."); exit;
-		
+             print_r("Thanks for reaching out. I'll get back to you.");
+
 			if($msg !== ''){
-				
+
 				$email_to = "mike@logikbox.com";
 				$email_subject = "Logikbox email";
-				
+
 				$headers = 'From: '.$email_from."\r\n".
-			
+
 			   'Reply-To: '.$email_from."\r\n" .
-				
+
 			   //'X-Mailer: PHP/' . phpversion();
-				
-			   @mail($email_to, $email_subject, $msg, $headers);  
+
+			   @mail($email_to, $email_subject, $msg, $headers);
 
 			}
-		
+
 		} else {
-			
+
 			print_r("Failed Robotic validation.");
-			
-			}  
-			
+
+			}
+
     }
 }
 
