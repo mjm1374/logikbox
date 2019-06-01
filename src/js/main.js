ddd9a4691332c9842bc0613cf8488f0ebeb06a51
Mike McAllister <mike@logikbox.com>
2019-06-01 16:13:23 -0400

    instagram video

--- src/js/main.js
+++ src/js/main.js
@@ -11,7 +11,6 @@ const days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Satur
 const months = ["January","Febuary","March","April","May","June","July","August","September","October","November","Decemeber"];
 	
 
-
 jQuery(document).ready(function($) {
 	var timelineBlocks = $('.cd-timeline-block'),
 		offset = 0.8;
@@ -76,7 +75,7 @@ function GetInstagram(max_id){
 	$.when(  $.get( 'https://api.instagram.com/v1/users/self/media/recent/', 
 	{ access_token: access_token, max_id: max_id } ) )
 	.then(function( result ) {
-		//console.log( "my results", result );
+		console.log( "my results", result );
 		var myPics =  result.data;
 		var NextMaxID = result.pagination.next_max_id;
 		//console.log(NextMaxID);
@@ -91,6 +90,7 @@ function GetInstagram(max_id){
 
 			for(i = 0; i < myPics.length; i++) {
 				let myString = "";
+				let isVideo = false;
 				let date = new Date(parseInt(myPics[i].created_time) * 1000);
 				let month = (date.getMonth()+1);
 				let day = date.getDate();
@@ -107,14 +107,26 @@ function GetInstagram(max_id){
 					location =  new setLocation(myPics[i].location.latitude, myPics[i].location.longitude, myPics[i].location.name );
 
 				}
+				if(myPics[i].videos != undefined){
+					isVideo = true;
+
+				}
 
 				myCaption =  myCaption.replace(/'/g, "&#8217;");
 
 				myString += "<div class='col-md-3 col-sm-6'>";
 				myString += "<div class='instagram__holder'>";
 				//myString += "<img class='instagram__pic' src='" + myPics[i].images.low_resolution.url + "' alt='" + myPics[i].caption.text + "' />";
-				myString += "<div class='instagram__pic' data-pic='" + myPics[i].images.standard_resolution.url + "' data-caption='" +  myCaption + "' style='background-image:url(" + myPics[i].images.standard_resolution.url + ");' />";
-				myString += "<div class='instagram__copy'>" + myCaption + "<br /><span class='instagram__date'>" + fullDate ;
+				
+				if(isVideo == false){
+					myString += "<div class='instagram__pic' data-pic='" + myPics[i].images.standard_resolution.url + "' data-caption='" +  myCaption + "' style='background-image:url(" + myPics[i].images.standard_resolution.url + ");'>";
+				 
+
+				}else{
+					myString += "<div class='instagram__pic' data-pic='" + myPics[i].images.standard_resolution.url + "' data-caption='" +  myCaption + "' >";
+					myString += "<div class='instagram__video' ><video autoplay muted loop><source src=" + myPics[i].videos.low_resolution.url + " type='video/mp4'></video> </div>";
+				}
+				myString += "</div><div class='instagram__copy'>" + myCaption + "<br /><span class='instagram__date'>" + fullDate ;
 				if(location.name != "" && location.name != undefined){
 					myString += " - " + location.name; 
 				}
