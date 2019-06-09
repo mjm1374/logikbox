5d32bb868abc046e178ecdebdffbe92114a651e8
Mike McAllister <mike@logikbox.com>
2019-06-09 08:37:48 -0400

    Mission info for multi mission flights

--- src/javascript/spaceX.js
+++ src/javascript/spaceX.js
@@ -4,34 +4,64 @@ function GetSpaceX(max_id){
 	.then(function( result ) {
         let launches = result;
         console.log(launches);
+        var promises = [];
 
         for(i=0; i < launches.length; i++){
             let target = $('#launchBlock' +i);
             let rocket = launches[i].rocket.rocket_id;
-            let payload = launches[i].rocket.second_stage.payloads[0].payload_type;
             let rocketImg = 'falcon9.png';
+            let rocketName = launches[i].rocket.rocket_name;
+            let payload = launches[i].rocket.second_stage.payloads[0].payload_type;
+            let missionID = '';
             let launchDate = timeConverter(launches[i].launch_date_unix);
-
+            let description = '';
+            
+            
+            if(launches[i].mission_id.length > 0){  missionID = launches[i].mission_id[0];}
             if(rocket == 'falconheavy') { rocketImg = 'falconheavy.png'; }
             if(payload.toLowerCase().indexOf("dragon") >= 0) { rocketImg = 'dragon.png'; }
-
-
             if(launches[i].details == '' || launches[i].details == null) {
-                launches[i].details = "<span class=''>There are no details available for this mission.</span>";
+                description =  $.ajax({url: 'https://api.spacexdata.com/v3/missions/' + missionID, success: function( mission ) {
+                    //console.log("mission",mission);
+                    details = mission.description;
+                    target.find(".launch__details").html(details);
+                   }});
+                promises.push(description);
+            } else{
+                description = launches[i].details;
+                target.find(".launch__details").html(description);
             }
             
-
             target.find(".launch__mission__name").html(launches[i].mission_name);
             target.find(".launch__rocket").html(launches[i].rocket.rocket_name);
             target.find(".launch__date").html(launchDate);
             target.find(".launch__site").html(launches[i].launch_site.site_name_long);
-            target.find(".launch__details").html(launches[i].details);
             target.find(".launch__img").attr('src','/img/spacex/' + rocketImg);
+            target.find(".launch__img").attr('alt', rocketName);
         }
+        $.when.apply(null, promises).done(function(){
+            //console.log('All done')
+         });
 
     }
     );
 } 
+
+function GetMission(missionID){
+    let details = "<span class=''>There are no details available for this mission2.</span>";
+
+    $.when(  $.get( 'https://api.spacexdata.com/v3/missions/' + missionID 
+                 ) )
+    .then(function( mission ) {
+        console.log("mission",mission.description);
+        details = mission.description;
+        return details;
+       }
+    );
+
+    
+
+}
 
 function timeConverter(UNIX_timestamp){
     var a = new Date(UNIX_timestamp * 1000);
