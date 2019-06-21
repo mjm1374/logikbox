c69a7b13531fe3b8a613f90136e910dbe58ff224
Mike McAllister <mike@logikbox.com>
2019-06-20 22:50:20 -0400

    matches modal title fix

--- src/modal-worldcup.php
+++ src/modal-worldcup.php
@@ -5,7 +5,7 @@
         <div class="modal-content">
             <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal">&times;</button>
-            <h4 class="modal-title modal-dynamic-title worldcup__modal--title">Matches</h4>
+            <h4 class="modal-title  worldcup__modal--title">Matches</h4>
             </div>
             <div class="modal-body worldcup__matches">
                 
