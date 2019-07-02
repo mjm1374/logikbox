121956dee6bd092226d65b335df2f781a20143f3
Mike McAllister <mike@logikbox.com>
2019-07-02 18:47:56 -0400

    Basic page and presets

---
+++ src/tardis/index.php
@@ -0,0 +1,196 @@
+<?php
+	//include_once("../controller/Controller.php");
+    //$controller = new Controller();
+?>
+
+	<!doctype html>
+	<html class="no-js"  lang="en">
+	<head>
+		        <meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+        <title>Tardis JS - Logikbox - Mike McAllister</title>
+
+        <meta name="description" content="A Lightweight Javascript Date/Time Converter and Formatter">
+        <meta name="viewport" content="width=device-width">
+        <link href="https://fonts.googleapis.com/css?family=Archivo|Permanent+Marker|Shadows+Into+Light|Teko" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Source+Code+Pro&display=swap" rel="stylesheet">
+        <link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+        <meta name="msapplication-TileColor" content="#da532c">
+        <meta name="theme-color" content="#ffffff">
+        
+        <script src="https://code.jquery.com/jquery-3.4.1.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="css/tardis.css">
+        <link rel="stylesheet" href="css/github-light.css">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+        <!-- Optional theme -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+        <!-- Latest compiled and minified JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+        <script src="javascript/main.js"></script>
+        <script src="javascript/tardis.js"></script>
+		<link rel="canonical" href="http://tardis.logikbox.com/index.php"/>
+	</head>
+	<body>
+         
+    <div class="tardis container">
+        <div class="row">
+            <div class="col-sm-12 tardis__header">
+                <img src="img/galefreyan.png" alt="tarids" />
+                <div class="tardis__logo"></div>
+            </div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8 tardis__copy">
+                <h1>Tardis JS</h1>
+                <P>nimis montes reprobo esca praemitto fere distineo. sollicitudin tempor suscipit huic aliquip vehicula interdico. aliquam os accumsan iustum luctus ridiculus. fermentum delenit demoveo vitae ligula exerci ac. paratus eget lacinia eros modo tempor. tego probo vehicula ad ligula lobortis.</p>
+
+                <h3>Install</h3>
+                <p>Via NPM: <span class="code">npm i lbx-tardis</span></p>
+                <p>or</p>
+                <p>Include tardis.js in your build directory or link to it directly</p>
+                <code>
+                     
+                        &lt;script type="text/javascript" src="tardis.js"&gt;&lt;/script&gt;
+                   
+                </code>
+                <p>Download <a href="javascript/tardis.js.zip" title="Downlaod Tardis JS">tardis.js</a></p>
+
+                <h3>Usage</h3>
+                <p>magna cogo lacus mauris laoreet. iustum aliquam comis fusce jus mi rutrum. zelus praemitto capto leo augue tempor. roto consectetuer sollicitudin ibidem nostra virtus. regula venio elit luptatum pellentesque probo typicus nisl. te rusticus consectetur regula pertineo verto roto pagus. libero exputo zelus saepius quae valetudo oppeto pagus.</p>
+
+                <h3>Filters</h3>
+                <p>letalis ludus nisl interdum mi sodales. mollis abluo metuo saepius duis risus sagittis. tempor aliquet ultricies opes orem dictum abigo. modo eligo mi viverra egestas accumsan. condimentum cras gravida lectus sagittis.</P>
+
+<div class="tardis__table tardis__filters" style="width: 100%;" >
+<div class="tardis__tableBody">
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+<div class="tardis__tableRow">
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+<div class="tardis__tableCell">&nbsp;</div>
+</div>
+</div>
+</div>
+<!-- tardis__table.com -->
+
+
+                <h3>Presets</h3>
+                <p>letalis ludus nisl interdum mi sodales. mollis abluo metuo saepius duis risus sagittis. tempor aliquet ultricies opes orem dictum abigo. modo eligo mi viverra egestas accumsan. condimentum cras gravida lectus sagittis.</P>
+                
+<code>
+<pre>
+<span class="pl-c1">tardis</span>.<span class="pl-en">ISO</span>(<span class="pl-smi">{date}</span>)                <span class="pl-c">// 2019-06-30</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">ShortDate</span>(<span class="pl-smi">{date}</span>)          <span class="pl-c">// 06/30/2019</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">LongDate</span>(<span class="pl-smi">{date}</span>)           <span class="pl-c">// Jun 30 2019</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">DayMonthDate</span>(<span class="pl-smi">{date}</span>)       <span class="pl-c">// Thursday,  Decemeber 1, 2005</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">MonthDateTime</span>(<span class="pl-smi">{date}</span>)      <span class="pl-c">// Decemeber 1, 2005 18:50</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">MonthDateTime12</span>(<span class="pl-smi">{date}</span>)    <span class="pl-c">// Decemeber 1, 2005 6:50 PM</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">MonthDate</span>(<span class="pl-smi">{date}</span>)          <span class="pl-c">// Decemeber 1, 2005</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">TimeOfDay</span>(<span class="pl-smi">{date}</span>)          <span class="pl-c">// 14:00:53</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">TimeOfDay12</span>(<span class="pl-smi">{date}</span>)        <span class="pl-c">// 2:01:31 PM</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">Year</span>(<span class="pl-smi">{date}</span>)               <span class="pl-c">// 2019</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">Month</span>(<span class="pl-smi">{date}</span>)              <span class="pl-c">// June</span>
+<span class="pl-c1">tardis</span>.<span class="pl-en">Day</span>(<span class="pl-smi">{date}</span>)                <span class="pl-c">// Sunday</span>
+</pre>
+</code>
+                
+
+                
+            </div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-2"></div>
+            <div class="col-sm-8">
+                <footer>
+                     Footer
+                    
+                </footer>
+            </div>
+            <div class="col-sm-2"></div>
+        </div>
+    </div>
+
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-38537843-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'UA-38537843-1');
+    </script>
+
+</body>
+</html>
