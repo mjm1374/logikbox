88b876956c10e6ea68034228bd5941e7d87bb3b5
Mike McAllister <mike@logikbox.com>
2019-07-02 19:16:21 -0400

    filters, not mobile yet

--- src/tardis/index.php
+++ src/tardis/index.php
@@ -65,80 +65,182 @@
                 <h3>Filters</h3>
                 <p>letalis ludus nisl interdum mi sodales. mollis abluo metuo saepius duis risus sagittis. tempor aliquet ultricies opes orem dictum abigo. modo eligo mi viverra egestas accumsan. condimentum cras gravida lectus sagittis.</P>
 
-<div class="tardis__table tardis__filters" style="width: 100%;" >
+<div class="tardis__table tardis__filters code" style="width: 100%;" >
 <div class="tardis__tableBody">
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
-<div class="tardis__tableRow">
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-<div class="tardis__tableCell">&nbsp;</div>
-</div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">YYYY</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">fullYear</span></div>
+    <div class="tardis__tableCell">2019</div>
+    <div class="tardis__tableCell">full 4 digit year</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">YYY</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">wordYear</span></div>
+    <div class="tardis__tableCell">two thousand and nineteen</div>
+    <div class="tardis__tableCell">full english 4 digit year</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">YY</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">shortYear</span></div>
+    <div class="tardis__tableCell">19</div>
+    <div class="tardis__tableCell">truncated string of YYYY</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">y</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">year</span></div>
+    <div class="tardis__tableCell">119</div>
+    <div class="tardis__tableCell">integer value, no leading 0, years from 1970</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">MMMM</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">fullMonth</span></div>
+    <div class="tardis__tableCell">June</div>
+    <div class="tardis__tableCell">name value of month from months array</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">MMM</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">shortMonth</span></div>
+    <div class="tardis__tableCell">Jun</div>
+    <div class="tardis__tableCell">months[date.getMonth()].substr(0, 3)</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">MM</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">MMonth</span></div>
+    <div class="tardis__tableCell">06</div>
+    <div class="tardis__tableCell">string value of month with leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">M</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">month</span></div>
+    <div class="tardis__tableCell">6</div>
+    <div class="tardis__tableCell">integer value, no leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">m</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">monthInt</span></div>
+    <div class="tardis__tableCell">6</div>
+    <div class="tardis__tableCell">integer value, no leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">DDDD</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">fullDay</span></div>
+    <div class="tardis__tableCell">Saturday</div>
+    <div class="tardis__tableCell">name value of day or week from Days Array</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">DDD</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDat</span>.<span class="pl-en">shortDay</span></div>
+    <div class="tardis__tableCell">Sat</div>
+    <div class="tardis__tableCell">truncated string of DDDD</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">DD</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">DDay</span></div>
+    <div class="tardis__tableCell">06</div>
+    <div class="tardis__tableCell">string value of D with leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">D</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">day</span></div>
+    <div class="tardis__tableCell">6</div>
+    <div class="tardis__tableCell">integer value, no leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">D</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">dayInt</span></div>
+    <div class="tardis__tableCell">1</div>
+    <div class="tardis__tableCell">Day of week counter (Sun = 0, Sat = 6)</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">HHHH</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">wordHour</span></div>
+    <div class="tardis__tableCell">nine</div>
+    <div class="tardis__tableCell">string value of english text for a number</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">HH</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">HHour</span></div>
+    <div class="tardis__tableCell">09</div>
+    <div class="tardis__tableCell">string value of H with leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">H</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">hour</span></div>
+    <div class="tardis__tableCell">4</div>
+    <div class="tardis__tableCell">integer value, no leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">h</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">hourInt</span></div>
+    <div class="tardis__tableCell">4</div>
+    <div class="tardis__tableCell">integer value, no leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">IIII</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">wordSec</span></div>
+    <div class="tardis__tableCell">twenty one</div>
+    <div class="tardis__tableCell">string value of english text for a number</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">II </div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">MMin</span></div>
+    <div class="tardis__tableCell">30</div>
+    <div class="tardis__tableCell">string value of I with leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">I</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">min</span></div>
+    <div class="tardis__tableCell">30</div>
+    <div class="tardis__tableCell">integer value, no leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">i</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">minInt</span></div>
+    <div class="tardis__tableCell">30</div>
+    <div class="tardis__tableCell">integer value, no leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">SSSS</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">wordMin</span></div>
+    <div class="tardis__tableCell">thirty three</div>
+    <div class="tardis__tableCell">string value of english text for a number</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">SS </div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">sec</span></div>
+    <div class="tardis__tableCell">05</div>
+    <div class="tardis__tableCell">string value of S with leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">S</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">sec</span></div>
+    <div class="tardis__tableCell">45</div>
+    <div class="tardis__tableCell">integer value, no leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">s</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">secInt</span></div>
+    <div class="tardis__tableCell">45</div>
+    <div class="tardis__tableCell">integer value, no leading 0</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">TT</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">hour</span></div>
+    <div class="tardis__tableCell">AM/PM</div>
+    <div class="tardis__tableCell">meridiem display - upper case </div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell">tt</div>
+    <div class="tardis__tableCell"><span class="pl-c1">thisDate</span>.<span class="pl-en">hour</span></div>
+    <div class="tardis__tableCell">am/pm</div>
+    <div class="tardis__tableCell">meridiem display -  lower case</div>
+    </div>
+    <div class="tardis__tableRow">
+    <div class="tardis__tableCell"></div>
+    <div class="tardis__tableCell"></div>
+    <div class="tardis__tableCell"></div>
+    <div class="tardis__tableCell"></div>
+    </div>
 </div>
 </div>
 <!-- tardis__table.com -->
@@ -194,3 +296,4 @@
 
 </body>
 </html>
+
