aa784668efb0d946293eb159b6ef27bc0a81e879
Mike McAllister <mike@logikbox.com>
2020-09-13 11:50:30 -0400

    Instagram feed running

--- src/javascript/instagram.js
+++ src/javascript/instagram.js
@@ -1,5 +1,12 @@
 class Instragram {
-    constructor(caption = '', date, isVideo = false, mediaURL, location, likes) {
+    constructor(
+        caption = '',
+        date,
+        isVideo = false,
+        mediaURL,
+        location,
+        likes
+    ) {
         this.caption = caption;
         this.date = date;
         this.isVideo = isVideo;
@@ -13,86 +20,100 @@ let instagramGallery = [];
 let instaGalPos = 0;
 
 function GetInstagram(max_id) {
-    $.when($.get('https://graph.instagram.com/me/media/', {
+    $.when(
+        $.get('https://graph.instagram.com/me/media/', {
             access_token: access_token,
-            max_id: max_id,
-            fields: 'id,username,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username'
-        }))
-        .then(function (result) {
-            console.log("my results", result);
-            var myPics = result.data;
-            // var NextMaxID = result.pagination.next_max_id;
+            after: max_id,
+            fields: 'id,username,caption,media_type,media_url,permalink,thumbnail_url,timestamp,username',
+        })
+    ).then(function (result) {
+        //console.log("my results", result);
+        let myPics = result.data;
+        let NextMaxID = result.paging.next;
 
-            // if (NextMaxID != undefined) {
-            //     $('#getMoreIstagram').data('nexturl', NextMaxID);
-            // } else {
-            //     $('#getMoreIstagram').hide();
-            // }ß
+        if (NextMaxID != undefined) {
+            NextMaxID = NextMaxID.slice(NextMaxID.indexOf("after=") + 6);
+            $('#getMoreIstagram').data('nexturl', NextMaxID);
+        } else {
+            $('#getMoreIstagram').hide();
+        }
 
-            for (let i = 0; i < myPics.length; i++) {
-                let myString = "";
-                let isVideo = false;
-                let date = myPics[i].timestamp; //tardis.DayMonthDate(myPics[i].timestamp);
-                let fullDate = date;
-                let location = new setLocation();
-                let myCaption = "";
-                let mediaURL = "";
-                let likes = null;
-                //let likes = myPics[i].likes.count;
+        for (let i = 0; i < myPics.length; i++) {
+            let myString = '';
+            let isVideo = false;
+            let date = new Date(myPics[i].timestamp);
+            let fullDate = tardis.DayMonthDate(date.toUTCString());
+            let location = new setLocation();
+            let myCaption = '';
+            let mediaURL = '';
+            let likes = null;
+            //let likes = myPics[i].likes.count;
 
-                if (myPics[i].caption != undefined || myPics[i].caption != null) {
-                    myCaption = myPics[i].caption;
-                } else {
-                    myCaption = "Untitled";
-                }
-                myCaption = myCaption.replace(/'/g, "&#8217;");
-                if (myPics[i].location != undefined) {
-                    location = new setLocation(myPics[i].location.latitude, myPics[i].location.longitude, myPics[i].location.name);
-                }
-                if (myPics[i].media_type == "VIDEO") {
-                    isVideo = true;
-                }
+            if (myPics[i].caption != undefined || myPics[i].caption != null) {
+                myCaption = myPics[i].caption;
+            } else {
+                myCaption = 'Untitled';
+            }
+            myCaption = myCaption.replace(/'/g, '&#8217;');
+            if (myPics[i].location != undefined) {
+                location = new setLocation(
+                    myPics[i].location.latitude,
+                    myPics[i].location.longitude,
+                    myPics[i].location.name
+                );
+            }
+            if (myPics[i].media_type == 'VIDEO') {
+                isVideo = true;
+            }
 
-                myString += "<div class='col-md-3 col-sm-6'>";
-                myString += "<div class='instagram__holder'>";
+            myString += "<div class='col-md-3 col-sm-6'>";
+            myString += "<div class='instagram__holder'>";
 
-                if (isVideo == false) {
-                    mediaURL = myPics[i].media_url;
-                    myString += `<div class='instagram__border gallery__modal' data-isVid='${isVideo}' data-pid='${instaGalPos}' data-pic='${mediaURL}' data-caption='${myCaption}' style='background-image:url(${mediaURL});'>`;
-                    //<div class='likebox'><span class='text-red'>&hearts;</span><span class='instagram__date likecnt'>${likes}</span></div>
-                } else {
-                    mediaURL = myPics[i].media_url;
-                    myString += `<div class='instagram__border gallery__modal' data-isVid='${isVideo}' data-pid='${instaGalPos}' data-vid='${mediaURL}' data-caption='${myCaption}'>`;
-                    //<div class='likebox'></div> <span class='text-red'>&hearts;</span><span class='instagram__date likecnt'>${likes}</span>
-                    myString += `<div class='instagram__video'><video autoplay muted loop><source src='${mediaURL}' type='video/mp4'></video></div>`;
-                }
-                myString += `</div><div class='instagram__copy'>${myCaption}<br /><span class='instagram__date'>${fullDate}`;
-                if (location.name != "" && location.name != undefined) {
-                    myString += ` - ${location.name}`;
-                }
-                myString += `</span></div>
+            if (isVideo == false) {
+                mediaURL = myPics[i].media_url;
+                myString += `<div class='instagram__border gallery__modal' data-isVid='${isVideo}' data-pid='${instaGalPos}' data-pic='${mediaURL}' data-caption='${myCaption}' style='background-image:url(${mediaURL});'>`;
+                //<div class='likebox'><span class='text-red'>&hearts;</span><span class='instagram__date likecnt'>${likes}</span></div>
+            } else {
+                mediaURL = myPics[i].media_url;
+                myString += `<div class='instagram__border gallery__modal' data-isVid='${isVideo}' data-pid='${instaGalPos}' data-vid='${mediaURL}' data-caption='${myCaption}'>`;
+                //<div class='likebox'></div> <span class='text-red'>&hearts;</span><span class='instagram__date likecnt'>${likes}</span>
+                myString += `<div class='instagram__video'><video autoplay muted loop><source src='${mediaURL}' type='video/mp4'></video></div>`;
+            }
+            myString += `</div><div class='instagram__copy'>${myCaption}<br /><span class='instagram__date'>${fullDate}`;
+            if (location.name != '' && location.name != undefined) {
+                myString += ` - ${location.name}`;
+            }
+            myString += `</span></div>
             </div></div>`;
 
-                instagramGallery.push(new Instragram(myCaption, date, isVideo, mediaURL, location, likes));
-                instaGalPos++;
+            instagramGallery.push(
+                new Instragram(
+                    myCaption,
+                    date,
+                    isVideo,
+                    mediaURL,
+                    location,
+                    likes
+                )
+            );
+            instaGalPos++;
 
-                $('.instagram').append(myString);
-
-            }
-        }).fail(function (err) {
-            console.log("Error: " + err.responseText);
-        });
+            $('.instagram').append(myString);
+        }
+    });
+    .fail(function (err) {
+        console.log("Error: " + err.responseText);
+    })
 }
 
 function getNextPosition(ind, dir) {
     let InstraArrayLen = instagramGallery.length;
     let nextPos = ind + dir;
     if (nextPos < 0) nextPos = InstraArrayLen - 1;
-    if (nextPos > (InstraArrayLen - 1)) nextPos = 0;
+    if (nextPos > InstraArrayLen - 1) nextPos = 0;
 
     return nextPos;
 }
-
 
 $(document).on('click', '.modal-btn', function (e) {
     let currebtPos = $(this).data('gotoPos');
@@ -101,7 +122,7 @@ $(document).on('click', '.modal-btn', function (e) {
     let srcUrl = instagramGallery[currebtPos].mediaURL;
     let caption = instagramGallery[currebtPos].caption;
     let alt = instagramGallery[currebtPos].alt;
-    if (alt == 'undefined' || alt == "") alt = caption;
+    if (alt == 'undefined' || alt == '') alt = caption;
     let isVideo = instagramGallery[currebtPos].isVideo;
     let video = document.getElementById('bigstagram--vid');
 
@@ -130,11 +151,10 @@ $(document).on('click', '.modal-btn', function (e) {
         $('#bigstagram').removeClass('hidden');
         $('#bigstagram').attr('src', srcUrl);
         $('#bigstagram').attr('alt', alt);
-
     }
 });
 
-$('#getMoreIstagram').on("click", function (e) {
+$('#getMoreIstagram').on('click', function (e) {
     e.preventDefault();
     var max_id = $('#getMoreIstagram').data('nexturl');
     GetInstagram(max_id);
@@ -148,7 +168,7 @@ $(document).on('click', '.pic__modal', function (e) {
     let srcUrl = $(this).data('pic');
     let caption = $(this).data('caption');
     let alt = $(this).data('alt');
-    if (alt == 'undefined' || alt == "") alt = caption;
+    if (alt == 'undefined' || alt == '') alt = caption;
 
     $('#bigstagram').removeClass('hidden');
     $('#bigstagram').attr('src', srcUrl);
@@ -176,10 +196,9 @@ $(document).on('click', '.gallery__modal', function (e) {
     let srcUrl = instagramGallery[currentPos].mediaURL;
     let caption = instagramGallery[currentPos].caption;
     let alt = instagramGallery[currentPos].alt;
-    if (alt == 'undefined' || alt == "") alt = caption;
+    if (alt == 'undefined' || alt == '') alt = caption;
     let isVideo = instagramGallery[currentPos].isVideo;
     let video = document.getElementById('bigstagram--vid');
-
 
     if (isVideo) {
         $('#bigstagram--vid').removeClass('hidden');
@@ -202,5 +221,4 @@ $(document).on('click', '.gallery__modal', function (e) {
     }
     $('#photoModal').modal('show');
     $('.modal-dynamic-title').html(caption);
-
 });
